d595c17b93f759ea16b8ec55383478faee642083
Khaledyasser88 <khaledyasser1632004@gmail.com>
2025-08-04 14:52:33 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { Route, Routes } from 'react-router'
-import './App.css'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import AllDoctors from './pages/AllDoctors'
-import Login from './pages/Login'
-import Doctor from './pages/Doctor'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
-import Myappointments from './pages/Myappointments.jsx'
-import Profile from './pages/Profile'
-
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import AllDoctors from "./pages/AllDoctors";
+import Login from "./pages/Login";
+import Doctor from "./pages/Doctor";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Myappointments from "./pages/Myappointments.jsx";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -18,17 +17,16 @@ function App() {
       <Header />
       <main className="flex-grow container pt-[80px] pb-[80px] mx-auto">
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/allDoctors' element={<AllDoctors/>}/>
-          <Route path='/login' element={<Login/> }/>
-          <Route path='/doctor/:id' element={<Doctor/>}/>
-          <Route path='/my-appointments' element={<Myappointments/>}/>
-          <Route path='/my-profile' element={<Profile/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/allDoctors" element={<AllDoctors />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/doctor/:id" element={<Doctor />} />
+          <Route path="/my-appointments" element={<Myappointments />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </main>
-
       <Footer />
     </div>
   );
