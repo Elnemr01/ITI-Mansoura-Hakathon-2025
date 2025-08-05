3081e9acad72a0cff2e60f6401810b5508d1de02
Ahmed Elnemr <elnemr21092003@gmail.com>
2025-08-05 15:03:47 +0300

    make scroll to top after click on doctor card

--- src/components/Doctorcard/DoctorCard.jsx
+++ src/components/Doctorcard/DoctorCard.jsx
@@ -3,8 +3,17 @@ import './card.css'
 import { Link } from 'react-router'
 
 const DoctorCard = ({doctor}) => {
+
+let goToTop = () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
     return (
-        <Link className='docCard' to={`/doctor/${doctor._id}`}>
+        <Link className='docCard' to={`/doctor/${doctor._id}`} onClick={()=> goToTop()}>
             <div className="image">
                 <img src={doctor.image} alt="check connection" loading='lazy'/>
             </div>
