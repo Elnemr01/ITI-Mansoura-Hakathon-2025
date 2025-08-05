159e1891cdb974eb65587051acdecdafb6f250f4
Ahmed Elnemr <elnemr21092003@gmail.com>
2025-08-05 14:53:03 +0300

    fix some style in home page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -144,9 +144,9 @@ const Profile = ({ userData }) => {
             {editable ? <button onClick={() => handleAfterEdit()}>save information</button>
                 : <button onClick={() => setEditable(!editable)}>edit</button>}
         </div>
-      </div>
-    
-  );
+    //   </div>
+
+    );
 };
 
 export default Profile;
