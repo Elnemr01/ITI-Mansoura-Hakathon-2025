b5d0291ab9c82ec90cc186d2eebbd35bf12c55aa
sameh231 <aljalys02@gmail.com>
2025-08-05 14:30:46 +0300

    some changes in porfile page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Speciality from '../components/Speciality/Speciality';
-import Hero from 'src/components/Hero/Hero.jsx';
+import Hero from '../components/hero/hero';
+// import Hero from 'src/components/Hero/.jsx';
 // import TopDoctors from '../components/topDoctors/TopDoctors';
 // import GoLoginSection from '../components/goLoginSection/GoLoginSection';
+
+
 
 const Home = () => {
   return (
     <div className='home'>
       <Hero />
       <Speciality />
+
       {/* <TopDoctors /> */}
       {/* <GoLoginSection /> */}
     </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './pageStyle/loginprofile.css';
 
-const Profile = () => {
-    return (
-        <div>
-            Profile
+
+ const Profile = () => {
+  return (
+      <div className="profile-card">
+       
+        <div className="profile-info">
+
+          <img src="src/assets/assets_frontend/upload_area.png" alt="profile" />
+          <h2>SAMEH</h2>
+          <hr/>
+
+           <div className="contactinfo">
+            <h3>CONTACT INFORMATION</h3>
+                      <p className="Email"> Email id: <span> ahmedmostafa@gmail.com</span></p>
+                      <p className="phone">Phone:<span> 000000000000</span> </p>
+                       <p className="address">Address:</p>
+           </div>
+
+           <div className="basicinfo">
+            <h3>BASIC INFORMATION</h3>
+                      <p className="gender">Gender:<span>Not Selected</span></p>
+                      <p className="gender">Berthday:<span>Not Selected</span></p>
+
+           </div>
+          <button className="edit-button">Edit</button>
         </div>
-    )
-}
+      </div>
+    
+  );
+};
 
-export default Profile
+export default Profile;
