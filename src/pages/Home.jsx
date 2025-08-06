b39d16d9e1fb3df6683d8181039f4e1ac1088a4e
Ahmed Elnemr <elnemr21092003@gmail.com>
2025-08-06 12:08:01 +0300

    add hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import GoLoginSection from "../components/goLoginSection/GoLoginSection";
-import Hero from "../components/Hero/Hero";
 import Specialisty from "../components/specialistySection/Specialisty";
 import TopDoctors from "../components/topDoctors/TopDoctors";
 
 const Home = () => {
-  return (
-    <div className="home">
-      <Hero />
-      <Specialisty />
-      <TopDoctors />
-      <GoLoginSection />
-    </div>
-  );
+    return (
+        <div className="home">
+        <Specialisty />
+        <TopDoctors />
+        <GoLoginSection />
+        </div>
+    );
 };
 
 export default Home;
