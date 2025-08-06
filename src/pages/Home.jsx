19c3aac26c13224dd910f076c252e1dcae1f30b2
Ahmed Elnemr <elnemr21092003@gmail.com>
2025-08-06 12:15:30 +0300

    edit main.jsx for deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import GoLoginSection from "../components/goLoginSection/GoLoginSection";
+import Hero from "../components/hero/Hero";
 import Specialisty from "../components/specialistySection/Specialisty";
 import TopDoctors from "../components/topDoctors/TopDoctors";
 
 const Home = () => {
     return (
         <div className="home">
+        <Hero/>
         <Specialisty />
         <TopDoctors />
         <GoLoginSection />
