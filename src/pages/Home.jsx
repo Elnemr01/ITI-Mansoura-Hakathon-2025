5ebfb5e0e5ca2be866ee785702ccae73fd7ead17
Abdalrahman_Gamal <ghozalabdo456@gmail.com>
2025-08-07 12:38:12 +0300

    synch with github

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import GoLoginSection from "../components/goLoginSection/GoLoginSection";
-import Hero from "../components/hero/HeroSection";
+import Hero from "../components/Hero/medhat";
 import Specialisty from "../components/specialistySection/Specialisty";
 import TopDoctors from "../components/topDoctors/TopDoctors";
 
