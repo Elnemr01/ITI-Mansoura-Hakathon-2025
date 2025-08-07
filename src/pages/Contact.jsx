15c66bced445015b7375d93179aebe1630dbeeaa
Ahmed Elnemr <elnemr21092003@gmail.com>
2025-08-07 15:07:33 +0300

    add animation to contact & about pages by belal

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import "./pageStyle/contactPage.css"
-import CommonTitle from '../components/commonTitle/CommonTitle'
-import { assets } from '../assets/assets_frontend/assets'
+import React from 'react';
+import "./pageStyle/contactPage.css";
+import CommonTitle from '../components/commonTitle/CommonTitle';
+import { assets } from '../assets/assets_frontend/assets';
+import { motion } from "framer-motion";
 
 const Contact = () => {
     return (
         <div className='contact'>
-            <CommonTitle word1={'contact'} word2={'us'} showP={false}/>
+            <CommonTitle word1={'contact'} word2={'us'} showP={false} />
+
             <div className="conLanding">
-                <div className="image">
-                    <img src={assets.contact_image} alt="check connection" loading='lazy'/>
-                </div>
-                <div className="info">
+                <motion.div
+                    className="image"
+                    initial={{ opacity: 0, x: -100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.8 }}
+                    viewport={{ once: false, amount: 0.3 }}
+                >
+                    <img src={assets.contact_image} alt="check connection" loading='lazy' />
+                </motion.div>
+
+                <motion.div
+                    className="info"
+                    initial={{ opacity: 0, x: 100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.8, delay: 0.2 }}
+                    viewport={{ once: false, amount: 0.3 }}
+                >
                     <h1>our store</h1>
                     <div className="site">
                         <p>54709 Willms Station</p>
@@ -24,10 +39,10 @@ const Contact = () => {
                     <h2>Careers at Forever</h2>
                     <p className="learnMore">Learn more about our teams and job openings.</p>
                     <button>explore jobs</button>
-                </div>
+                </motion.div>
             </div>
         </div>
     )
 }
 
-export default Contact
+export default Contact;
