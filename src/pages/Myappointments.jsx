ece0c67a755c9662c094f587a7a8044a1fcfc123
Abdalrahman_Gamal <ghozalabdo456@gmail.com>
2025-08-09 19:07:45 +0300

    fix animation in my-appointment page

--- src/pages/Myappointments.jsx
+++ src/pages/Myappointments.jsx
@@ -20,7 +20,7 @@ const Myappointments = () => {
       initial={{ opacity: 0, y: 60 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
-      viewport={{ once: false, amount: 0.3 }}>
+      viewport={{ once: false, amount: 0 }}>
       <section className="myAppointments">
         <h2 className="pageName">my appointments</h2>
 
